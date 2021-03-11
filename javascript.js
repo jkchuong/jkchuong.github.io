b4919c84978ce720c40ea7d75cc6b264ee40fa61
jkchuong <jkchuong@gmail.com>
2021-03-11 00:58:35 +0000

    added portfolio template

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 // JavaScript Documentwindow.addEventListener('DOMContentLoaded', () => {
 
+// Show portfolio items
 function openPortfolio(portfolioItem){
 	var i;
 	var x = document.getElementsByClassName("portfolio-items");
@@ -12,6 +13,7 @@ function openPortfolio(portfolioItem){
 	activatedButton(portfolioItem.concat("button"));
 }
 
+// Activate portfolio button
 function activatedButton(portfolioButton){
 	var i;
 		var x = document.getElementsByClassName("portfolio-button");
@@ -20,5 +22,41 @@ function activatedButton(portfolioButton){
 	}
 	
 	var element = document.getElementById(portfolioButton);
-	element.classList.add("activated");
+	element.classList.add("portfolio-activated");
 }
+
+// Animation on scroll
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty(
+      "--scroll",
+      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+    );
+  },
+  false
+);
+
+// Scrollspy
+(function() {
+  'use strict';
+
+  var section = document.querySelectorAll(".section");
+  var sections = {};
+  var i = 0;
+
+  Array.prototype.forEach.call(section, function(e) {
+    sections[e.id] = e.offsetTop;
+  });
+
+  window.onscroll = function() {
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (i in sections) {
+      if (sections[i] <= scrollPosition) {
+        document.querySelector('.active').setAttribute('class', ' ');
+        document.querySelector('a[href*=' + i + ']').setAttribute('class', '.sidebar-activated');
+      }
+    }
+  };
+})();
